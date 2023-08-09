fcd1a82fc497ca227d9b6a8d0b57329e85ec8945
mason tucker <mason@listella.com>
2023-08-09 18:34:11 -0400

    feat: Fix our block statement evaluation to allow for nested if execution. Added return statement evaluation, and test suite. Added new return value object as well.

--- object/object.go
+++ object/object.go
@@ -5,9 +5,10 @@ import "fmt"
 type ObjectType string
 
 const (
-	INTEGER_OBJ = "INTEGER"
-	BOOLEAN_OBJ = "BOOLEAN"
-	NULL_OBJ    = "NULL"
+	INTEGER_OBJ      = "INTEGER"
+	BOOLEAN_OBJ      = "BOOLEAN"
+	NULL_OBJ         = "NULL"
+	RETURN_VALUE_OBJ = "RETURN"
 )
 
 type Object interface {
@@ -33,3 +34,10 @@ type Null struct{}
 
 func (n *Null) Inspect() string  { return "null" }
 func (n *Null) Type() ObjectType { return NULL_OBJ }
+
+type ReturnValue struct {
+	Value Object
+}
+
+func (rv *ReturnValue) Inspect() string  { return RETURN_VALUE_OBJ }
+func (rv *ReturnValue) Type() ObjectType { return rv.Value.Type() }
